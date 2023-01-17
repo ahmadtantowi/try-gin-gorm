21f24f0c14bbda7a720d74d4338649a8a473a8aa
Ahmad Tantowi <ahmadtantowi@outlook.com>
2023-01-17 14:19:53 +0700

    Can find a book with id

--- controllers/bookHandler.go
+++ controllers/bookHandler.go
@@ -38,3 +38,14 @@ func CreateBook(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, gin.H{"data": book})
 }
+
+// get a book
+func FindBook(ctx *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id=?", ctx.Param("id")).First(&book).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": book})
+}
